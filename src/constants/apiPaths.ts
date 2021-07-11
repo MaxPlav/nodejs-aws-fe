12497470e80a662cf1ef200e34dd9365097ba389
Max Plavinskiy <max.plavinskiy@gmail.com>
2021-07-11 15:36:45 +0300

    Task 9: set bff service url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: 'https://dnlo58ogw8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5ylpe6sy93.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://dnlo58ogw8.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'http://maxplav-bff-api-dev.eu-west-1.elasticbeanstalk.com',
   cart: 'http://maxplav-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
